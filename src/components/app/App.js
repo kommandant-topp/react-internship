f48858957108c63723fc33bfd63e89707968818f
kommandant <der.commandant@gmail.com>
2019-08-06 17:34:14 +0300

    lesson 5 commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,111 +1,229 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../../assets/components/app/main.css';
 
-const App = () => {
+export default class App extends Component{
+    constructor() {
+        super();
 
-  const project_name_block = <div className="logo-text">
-        <span>React</span>Internship
-      </div>;
+        this.ID = 0;
+        this.menuID = 0;
 
-  const left_top_menu_1 = 'название',
-      left_top_menu_2 = 'краткое описание',
-      left_top_menu_3 = 'рабочая область',
-      left_top_menu_4 = 'ссылки';
+        this.createItem = (year, label) => {
+          return {
+              id: this.ID++,
+              year: year,
+              label: label
+          }
+        };
 
-  const left_top_menu_href_1 = 'project-name',
-      left_top_menu_href_2 = 'short-desc',
-      left_top_menu_href_3 = 'working-area',
-      left_top_menu_href_4 = 'footer-links';
+        this.addMenuElement = (title, href) => {
+          return {
+            id: this.menuID++,
+            title: title,
+            href: href
+          }
+        };
 
-  const right_top_menu_1 = 'вход',
-      right_top_menu_2 = 'FAQ';
+        this.state = {
+          leftTopMenu: [
+              this.addMenuElement('название', 'project-name'),
+              this.addMenuElement('краткое описание', 'short-desc'),
+              this.addMenuElement('рабочая область', 'working-area'),
+              this.addMenuElement('ссылки', 'footer-links'),
+          ],
 
-  const right_top_menu_href_1 = 'login',
-    right_top_menu_href_2 = 'faq';
+          rightTopMenu: [
+              this.addMenuElement('вход', 'login'),
+              this.addMenuElement('FAQ', 'faq'),
+          ],
 
-  const lesson_name = 'ReactInternship.Lesson4';
+          footerLinks: [
+              this.addMenuElement('оригинал-макет', 'https://handprinter.org/pages/home'),
+              this.addMenuElement('Дуванов Алексей', 'https://github.com/kommandant-topp/react-internship'),
+          ],
 
-  const screen_cast_element_1 = 'Введение, компонентный подход',
-      screen_cast_element_2 = 'Настройка окружения, create-react-app',
-      screen_cast_element_3 = 'JSX - синтаксис для разметки в React.js',
-      screen_cast_element_4 = 'React-компонент, передача данных',
-      screen_cast_element_5 = 'Интерактивность, состояние компонентов',
-      screen_cast_element_6 = 'Virtual DOM - основной алгоритм React.js',
-      screen_cast_element_7 = 'Отображение массивов, смысл аттрибута key',
-      screen_cast_element_8 = 'Оформление компонентов, работа с CSS';
+          bio: [
+            this.createItem(1999, 'ЦНТУ'),
+            this.createItem(2004, 'ИВЦ ЦНТУ'),
+            this.createItem(2005, 'YasenDesign'),
+            this.createItem(2006, 'KoreDesign'),
+            this.createItem(2007, 'WebHata'),
+            this.createItem(2013, 'freelance'),
+            this.createItem(2017, 'ROMAD LLC')
+          ],
+          sortDest: -1
+        };
 
-  const footer_links_text_1 = 'оригинал-макет',
-      footer_links_text_2 = 'Дуванов Алексей';
+        this.leftTopMenu = this.state.leftTopMenu.map(({id, title, href}) => {
+          return (
+                <li key={id}>
+                  <a href={'#' + href}>{title}</a>
+                </li>
+          );
+        });
 
-  const footer_links_href_1 = 'https://handprinter.org/pages/home',
-      footer_links_href_2 = 'https://github.com/kommandant-topp/react-internship';
+        this.rightTopMenu = this.state.rightTopMenu.map(({id, title, href}) => {
+            return (
+                  <li key={id}>
+                    <a href={'#' + href}>{title}</a>
+                  </li>
+            );
+        });
 
-  return(
-      <div>
-        <header>
+        this.mainList = () => {
+            return this.state.bio.map(({id, year, label}) => {
+                return(
+                    <li key={id}>{year}&nbsp;{label}</li>
+                );
+            });
+        };
 
-          <div className="top-block">
-              {project_name_block}
-            <div className="main-menu">
-              <ul>
-                <li><a href={'#' + left_top_menu_href_1}>{left_top_menu_1}</a></li>
-                <li><a href={'#' + left_top_menu_href_2}>{left_top_menu_2}</a></li>
-                <li><a href={'#' + left_top_menu_href_3}>{left_top_menu_3}</a></li>
-                <li><a href={'#' + left_top_menu_href_4}>{left_top_menu_4}</a></li>
-              </ul>
-            </div>
-            <div className="right-menu">
-              <ul>
-                <li><a href={'#' + right_top_menu_href_1}>{right_top_menu_1}</a></li>
-                <li><a href={'#' + right_top_menu_href_2}>{right_top_menu_2}</a></li>
-              </ul>
-            </div>
-          </div>
+        this.footerLinks = this.state.footerLinks.map(({id, title, href}) => {
+            return (
+               <a href={'#' + href} target="_blank" rel="noopener noreferrer" key={id}>{title}</a>
+            );
+        });
 
-        </header>
-        <main>
-          <div className="main-block">
-            <section>
-              <div className="name-area">
-                <h3 id="project-name">{lesson_name}</h3>
+        this.addListItem = () => {
+            this.setState(({bio}) => {
+              return {
+                bio: [
+                    ...bio,
+                    this.createItem(1988, 'Школа')
+                ]
+              }
+            });
+        };
+
+        this.deleteListItem = () => {
+            this.setState(({bio}) => {
+                let list = bio.slice();
+
+                return {
+                    bio: list.slice(0, list.length - 1)
+                }
+            });
+        };
+
+        this.sortList = () => {
+          this.setState(({bio, sortDest}) => {
+              let list = bio.slice();
+
+              list = this.commonSortList(bio, sortDest);
+              //list = this.customSortList(bio, sortDest);
+
+              return {
+                bio: list,
+                sortDest: sortDest * -1
+              }
+          });
+        };
+
+        this.commonSortList = (arr, dest) => {
+            return arr.sort(
+                (a, b) => {
+                    return (a.year - b.year) * dest;
+                }
+            );
+        };
+
+        this.customSortList = (arr, dest) =>
+        {
+            let arrayLength = arr.length, delta = Math.floor(arrayLength/2);
+
+            let getDest = (dest, deltaValue, currentValue) => {
+              if (dest > 0){
+                  return deltaValue > currentValue;
+              } else {
+                  return deltaValue < currentValue;
+              }
+            };
+
+            while (delta > 0)
+            {
+                for (let mainIndex = 0; mainIndex < arrayLength; mainIndex++)
+                {
+                    let k = mainIndex,
+                        currentValue = arr[mainIndex];
+
+                    while (k >= delta && getDest(dest, arr[k-delta].year, currentValue.year))
+                    {
+                        arr[k] = arr[k-delta];
+                        k -= delta;
+                    }
+
+                    arr[k] = currentValue;
+                }
+
+                delta = Math.floor(delta/2);
+            }
+            return arr;
+        }
+
+    }
+
+    render(){
+      return(
+          <div class="main-container">
+            <header>
+
+              <div className="top-block">
+                <div className="logo-text">
+                  <span>React</span>Internship
+                </div>
+                <div className="main-menu">
+                  <ul>
+                      {this.leftTopMenu}
+                  </ul>
+                </div>
+                <div className="right-menu">
+                  <ul>
+                      {this.rightTopMenu}
+                  </ul>
+                </div>
               </div>
-            </section>
-            <section>
-              <div className="desc-area">
-                <h3 id="short-desc">Краткое описание</h3>
 
-                <p>
-                  Ознакомиться с node и npm, получить базовые понятия о том что такое react, развернуть первый проект с помощью react cli
-                </p>
-              </div>
+            </header>
+            <main>
+              <div className="main-block">
+                <section>
+                  <div className="name-area">
+                    <h3 id="project-name">ReactInternship.Lesson5</h3>
+                  </div>
+                </section>
+                <section>
+                  <div className="desc-area">
+                    <h3 id="short-desc">Краткое описание</h3>
 
-            </section>
-            <section>
-              <div className="main-area">
-                <h3 id="working-area">Рабочая область</h3>
-                <p>Выпуски скринкаста</p>
-                <ol className="main-list">
-                  <li>{screen_cast_element_1}</li>
-                  <li>{screen_cast_element_2}</li>
-                  <li>{screen_cast_element_3}</li>
-                  <li>{screen_cast_element_4}</li>
-                  <li>{screen_cast_element_5}</li>
-                  <li>{screen_cast_element_6}</li>
-                  <li>{screen_cast_element_7}</li>
-                  <li>{screen_cast_element_8}</li>
-                </ol>
+                    <p>
+                      основы js, массивы
+                    </p>
+                  </div>
+
+                </section>
+                <section>
+                  <div className="main-area">
+                    <h3 id="working-area">Рабочая область</h3>
+                    <p>био</p>
+                    <ol className="main-list" onClick={this.sortList}>
+                        {this.mainList()}
+                    </ol>
+                    <div className="control-buttons">
+                      <button onClick={this.addListItem}>добавить поле</button>
+                      <button onClick={this.deleteListItem}>удалить поле</button>
+                    </div>
+                  </div>
+                </section>
               </div>
-            </section>
+            </main>
+            <footer>
+              <div className="bottom-block">
+                  {this.footerLinks}
+              </div>
+            </footer>
           </div>
-        </main>
-        <footer>
-          <div className="bottom-block">
-            <a href={footer_links_href_1} target="_blank" rel="noopener noreferrer" >{footer_links_text_1}</a>
-            <a href={footer_links_href_2} target="_blank" rel="noopener noreferrer" >{footer_links_text_2}</a>
-          </div>
-        </footer>
-      </div>
-  );
+      );
+    }
 };
 
-export default App;
+
